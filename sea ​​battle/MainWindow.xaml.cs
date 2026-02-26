d6b3b7f08c251010d698329ffa197ac47b140438
ZadoienkoRoman <ipz243_zra@student.ztu.edu.ua>
2026-02-26 16:36:24 +0200

    Upload without heavy audio

---
+++ sea ​​battle/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using sea_battle.Views;
+using System.Windows;
+
+namespace sea_battle
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContainer.Children.Add(new MenuView());
+        }
+    }
+}
---
+++ sea ​​battle/Models/Ship.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace sea_battle.Models
+{
+    public class Ship
+    {
+        public List<Cell> Cells { get; set; } = new List<Cell>();
+
+        public bool IsSunk => Cells.All(c => c.Status == CellStatus.Hit || c.Status == CellStatus.Destroyed);
+
+        public void MarkAsDestroyed()
+        {
+            foreach (var cell in Cells)
+                cell.Status = CellStatus.Destroyed;
+        }
+    }
+}
