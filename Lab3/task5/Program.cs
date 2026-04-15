4b59ac4de6e7509b2cdf5e3aa6cfaae911e0d280
Roman9492 <ipz243_zra@student.ztu.edu.ua>
2026-04-15 20:11:55 +0300

    Додано паттерн Ітератор (DFS та BFS)
    Реалізовано паттерн Ітератор для обходу HTML-дерева двома способами: в глибину (DFS) за допомогою стеку та в ширину (BFS) за допомогою черги.

--- Lab3/task5/Program.cs
+++ Lab3/task5/Program.cs
@@ -4,10 +4,17 @@ using System.Text;
 
 namespace task5
 {
+    public interface IIterator
+    {
+        LightNode Next();
+        bool HasNext();
+    }
+
     public abstract class LightNode
     {
         public abstract string OuterHTML();
         public abstract string InnerHTML();
+        public virtual List<LightNode> GetChildren() => new List<LightNode>();
         public virtual void OnBeforeRender() { }
         public virtual void OnAfterRender() { }
     }
@@ -22,7 +29,7 @@ namespace task5
 
     public class LightElementNode : LightNode
     {
-        protected string _tagName; 
+        protected string _tagName;
         private string _displayType; 
         private bool _isSelfClosing;
         private List<string> _cssClasses = new List<string>();
@@ -37,45 +44,50 @@ namespace task5
         }
 
         public void AddChild(LightNode node) => _children.Add(node);
+        public override List<LightNode> GetChildren() => _children;
 
         public override string InnerHTML()
         {
             StringBuilder sb = new StringBuilder();
-            foreach (var child in _children)
-            {
-                sb.Append(child.OuterHTML());
-            }
+            foreach (var child in _children) sb.Append(child.OuterHTML());
             return sb.ToString();
         }
 
         public override string OuterHTML()
         {
-            OnBeforeRender(); 
-
+            OnBeforeRender();
             string classes = _cssClasses.Count > 0 ? $" class=\"{string.Join(" ", _cssClasses)}\"" : "";
-            string result;
-
-            if (_isSelfClosing)
-            {
-                result = $"<{_tagName}{classes} />";
-            }
-            else
-            {
-                result = $"<{_tagName}{classes}>\n  {InnerHTML()}\n</{_tagName}>";
-            }
-
+            string result = _isSelfClosing ? $"<{_tagName}{classes} />" : $"<{_tagName}{classes}>\n  {InnerHTML()}\n</{_tagName}>";
             OnAfterRender();
             return result;
         }
     }
 
-    public class LoggingElementNode : LightElementNode
+    public class DepthFirstIterator : IIterator
     {
-        public LoggingElementNode(string tagName, string displayType, bool isSelfClosing) 
-            : base(tagName, displayType, isSelfClosing) { }
+        private Stack<LightNode> _stack = new Stack<LightNode>();
+        public DepthFirstIterator(LightNode root) => _stack.Push(root);
+        public bool HasNext() => _stack.Count > 0;
+        public LightNode Next()
+        {
+            var node = _stack.Pop();
+            var children = node.GetChildren();
+            for (int i = children.Count - 1; i >= 0; i--) _stack.Push(children[i]);
+            return node;
+        }
+    }
 
-        public override void OnBeforeRender() => Console.WriteLine($"[LOG]: Початок рендерингу <{_tagName}>");
-        public override void OnAfterRender() => Console.WriteLine($"[LOG]: Завершено рендеринг <{_tagName}>");
+    public class BreadthFirstIterator : IIterator
+    {
+        private Queue<LightNode> _queue = new Queue<LightNode>();
+        public BreadthFirstIterator(LightNode root) => _queue.Enqueue(root);
+        public bool HasNext() => _queue.Count > 0;
+        public LightNode Next()
+        {
+            var node = _queue.Dequeue();
+            foreach (var child in node.GetChildren()) _queue.Enqueue(child);
+            return node;
+        }
     }
 
     class Program
@@ -83,11 +95,21 @@ namespace task5
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            
-            var list = new LoggingElementNode("ul", "block", false);
-            list.AddChild(new LightTextNode("Демонстрація Template Method"));
 
-            Console.WriteLine(list.OuterHTML());
+            var list = new LightElementNode("ul", "block", false);
+            var item1 = new LightElementNode("li", "block", false);
+            item1.AddChild(new LightTextNode("Перший"));
+            list.AddChild(item1);
+            list.AddChild(new LightElementNode("li", "block", false));
+
+            Console.WriteLine("--- Тест Ітератора DFS (В глибину) ---");
+            var dfs = new DepthFirstIterator(list);
+            while (dfs.HasNext()) Console.WriteLine("Вузол: " + dfs.Next().GetType().Name);
+
+            Console.WriteLine("\n--- Тест Ітератора BFS (В ширину) ---");
+            var bfs = new BreadthFirstIterator(list);
+            while (bfs.HasNext()) Console.WriteLine("Вузол: " + bfs.Next().GetType().Name);
+
             Console.ReadKey();
         }
     }
